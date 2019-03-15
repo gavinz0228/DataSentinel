06c7b82e6fbe772cfeffc9c5902e209ba8e0d643
gavin <gavinz0228@gmail.com>
2019-03-14 23:28:03 -0400

    imporved python example

--- examples/python/data_accessor.py
+++ examples/python/data_accessor.py
@@ -2,24 +2,26 @@ import requests
 import json
 class DataAccessor(object):
     def __init__(self, base_url):
-        self.base_url = base_url
+
+        if not base_url.endswith('/'):
+            self.base_url = self.add_slash(base_url)
+        else:
+            self.base_url = base_url
+
         self.token = None
         self.post_headers = {'Content-Type': 'application/json'}
         self.get_headers = {'Content-Type': 'application/json'}
-        if not self.base_url.endswith('/'):
-            self.base_url += '/'
 
     def post(self, url, data):
         if self.token and 'Authorization' not in self.post_headers:
             self.post_headers['Authorization'] = 'Bearer ' + self.token 
-        response = requests.post(self.base_url + url, json = data, headers = self.post_headers)
+        response = requests.post(self.base_url + self.add_slash(url), json = data, headers = self.post_headers)
         return response.json()
 
     def get(self, url, filter_str):
         if self.token and 'Authorization' not in self.get_headers:
             self.get_headers['Authorization'] = 'Bearer ' + self.token 
-        print(self.base_url + url + "/" + json.dumps(filter_str))
-        response = requests.get(self.base_url + url + "/" + json.dumps(filter_str), headers = self.get_headers)
+        response = requests.get(self.base_url + self.add_slash(url) + json.dumps(filter_str), headers = self.get_headers)
         return response.json()
 
     def auth(self, user_name, password):
@@ -27,7 +29,11 @@ class DataAccessor(object):
         self.token = resp_json['token']
         if not self.token:
             raise Exception('Wrong user name or password.')
-            
+    def add_slash(self, url):
+        if not url.endswith('/'):
+            return url + '/'
+        else:
+            return url
 if __name__ == "__main__":
     da = DataAccessor("http://localhost/")
     da.auth("gavin0228", "8524560")
